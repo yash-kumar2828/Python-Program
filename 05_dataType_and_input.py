ebc0c7d8c29c63afbe4d3fd53eb77fddac99cfb2
yash kumar <you@example.com>
2025-07-15 16:03:00 +0530

    data type and input use

---
+++ 05_dataType_and_input.py
@@ -0,0 +1,19 @@
+a=1
+b=3.33
+c="yash"
+d=True
+e=None
+f="yash"
+print("a is =",type(a))
+print("b is =",type(b))
+print("c is =",type(c))
+print("d is =",type(d))
+print("e is =",type(e))
+print("f is =",type(f))
+
+# input function 
+name=input("enter a name=")
+print(name)
+a=int(input("enter a number="))
+b=int(input("enter a number="))
+print("sum=",a+b)
