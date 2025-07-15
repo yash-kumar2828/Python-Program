6f451818fecb173222a0a52ed34003a8baca7ddb
yash kumar <you@example.com>
2025-07-15 16:06:38 +0530

    meaning

---
+++ 17_meaning.py
@@ -0,0 +1,9 @@
+# wap to create a dictionary of hindi words with values as their english translation . provide user with an option to look it up
+
+words={
+    "madad":"help",
+    "kursi":"chair",
+    "billi":"cat"
+}
+word=input("enter the word you want meaning of: ")
+print(words[word])
