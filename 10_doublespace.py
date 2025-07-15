e4a38c88c524cb3c4ded40b9de389dd26ef51f7b
yash kumar <you@example.com>
2025-07-15 16:04:19 +0530

    use of double space

---
+++ 10_doublespace.py
@@ -0,0 +1,10 @@
+
+name="yash is a good  boy and"
+print(name.find("  "))
+
+name1="yash is a good  boy and"
+print(name1.replace("  "," "))
+
+# escape sequence
+letter="dear yash, \n\tthis python course is nice. \nthanks!"
+print(letter)
