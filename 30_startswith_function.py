0677b29f41296bcc5ca2e038926d420c8c5202fb
yash kumar <you@example.com>
2025-07-15 16:12:09 +0530

    use of startswith

---
+++ 30_startswith_function.py
@@ -0,0 +1,8 @@
+# wap to greet all the person names stored in a list and which starts with S 
+l=["yash","mukesh","ritik","sachin","sohan"]
+for name in l:
+    if(name.startswith("s")):
+        print(f"hello {name}")
+
+
+
