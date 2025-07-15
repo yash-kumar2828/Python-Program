050f7eac6608471821af68e9f28d3eb1910c547e
yash kumar <you@example.com>
2025-07-15 16:06:02 +0530

    create a tuple

---
+++ 15_tuples.py
@@ -0,0 +1,14 @@
+
+a=()                                                                      
+a=(1,)
+a=(4,7,4,"yash",7.4,False)
+print(a)
+print(type(a))
+
+# tupple method
+a1=(4,7,4,10,29,39,4)
+no=a1.count(4)
+print(no)
+no=a1.index(39)
+print(no)
+print(len(a1))
