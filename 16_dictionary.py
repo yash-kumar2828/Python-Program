a8442e599ebdd85faf6d9f92086d6ac8f789f903
yash kumar <you@example.com>
2025-07-15 16:06:21 +0530

    create a dictionary

---
+++ 16_dictionary.py
@@ -0,0 +1,32 @@
+# example of dictionary
+marks={
+    "yash":100,
+    "kalpana":95
+}
+print(marks)
+print(type(marks))
+print(marks["kalpana"])
+
+
+# dictionary method
+a={
+    "name":"yash",
+    "from":"India",
+    "marks":[98,89,87,78,95]
+}
+
+print(a.items())
+print(a.keys())
+print(a.values())
+
+a.update({"from":"Bihar"})
+print(a)
+
+print(a.get("marks"))
+
+a.pop("name")
+print(a)
+
+a.popitem()
+print(a)
+
